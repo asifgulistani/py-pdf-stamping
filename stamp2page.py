19a38bf63ac3ef7a743f79360acd528a23bad194
OptiPlex 7070 <a.gulistani@mfa.af>
2021-06-27 11:21:21 +0430

    new stamp position added and form field flattenned

--- stamp2page.py
+++ stamp2page.py
@@ -1,6 +1,7 @@
 import glob, os
 import sys
 import PyPDF2
+from PyPDF2.generic import NameObject, NumberObject
 
 
 dirname = os.path.dirname(__file__) #<-- absolute dir the script is in
@@ -8,7 +9,7 @@ dirname = os.path.dirname(__file__) #<-- absolute dir the script is in
 # rel_path = "2091/data.txt"
 # abs_file_path = os.path.join(dirname, rel_path)
 
-watermark_file = os.path.join(dirname, "stamp_with_sign_2page.pdf")
+watermark_file = os.path.join(dirname, "stamp_1page.pdf")
 
 # print(dirname + "/files")
 for file in os.listdir(dirname + "/files"):
@@ -28,28 +29,36 @@ for file in os.listdir(dirname + "/files"):
                 
                 # get first page of the original PDF
                 first_page = pdf.getPage(0)
-                second_page = pdf.getPage(1)
+                # second_page = pdf.getPage(1)
+
+                # print(first_page['/Annots']);
+                # make readonly the for sending to mission
+                for j in range(0, len(first_page['/Annots'])):
+                    writer_annot = first_page['/Annots'][j].getObject()
+                    writer_annot.update({
+                        NameObject("/Ff"): NumberObject(1)   # make ReadOnly
+                    })
                 
                 # get first page of the watermark PDF
                 first_page_watermark = watermark.getPage(0)
-                second_page_watermark = watermark.getPage(1)
+                #second_page_watermark = watermark.getPage(1)
                 
                 # merge the two pages
                 first_page.mergePage(first_page_watermark)
 
-                second_page.mergePage(second_page_watermark)
+                # second_page.mergePage(second_page_watermark)
                 
                 # create a pdf writer object for the output file
                 pdf_writer = PyPDF2.PdfFileWriter()
                 
                 # add page
                 pdf_writer.addPage(first_page)
-                pdf_writer.addPage(second_page)
+                # pdf_writer.addPage(second_page)
                 
                 with open(output_file, "wb") as filehandle_output:
                     # write the watermarked file to the new file
                     pdf_writer.write(filehandle_output)
 
-print('done');
+print('done')
 sys.exit()
 
